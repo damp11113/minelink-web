25531e72ed07743f3593bf371347dc73c7315a01
damp11113 <64675096+damp11113@users.noreply.github.com>
2022-02-07 22:23:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -10,16 +10,9 @@ Swal.fire({
     showLoaderOnConfirm: true,
     preConfirm: (login) => {
         return fetch(`https://1dab-171-6-245-3.ngrok.io/v1/add/${login}`)
-            .catch(response => {
-                Swal.fire({
-                    title: 'Success!',
-                    text: 'You have been added to the bot!',
-                    icon: 'success',
-                }
-            )
-            .then(() => {
-                window.location.reload();
-            }
+        .then(response => response.json())
+        .catch(error => {
+            Swal.fire({title: 'Success!', text: 'You have been added to the server!', type: 'success'})
         })
     },
 })
