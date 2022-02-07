1072bfd8c40431f06611c691791a8eedea305183
damp11113 <64675096+damp11113@users.noreply.github.com>
2022-02-07 22:10:21 +0700

    web

--- script.js
+++ script.js
@@ -10,26 +10,11 @@ Swal.fire({
     showLoaderOnConfirm: true,
     preConfirm: (login) => {
         return fetch(`https://1dab-171-6-245-3.ngrok.io/v1/add/${login}`)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error(response.statusText)
+                Swal.fire({
+                    title: 'Success!',
+                    text: 'You have been added to the bot!',
+                    icon: 'success',
                 }
-                return response.json()
-            })
-            .catch(error => {
-            Swal.showValidationMessage(
-                `Request failed: ${error}`
             )
-        })
-    }
-}).then((result) => {
-    if (result.isConfirmed) {
-        Swal.fire({
-            title: 'Success!',
-            text: 'You have been added to the bot!',
-            icon: 'success',
-        }
-        )
-    }
-}
-)
+    },
+})
