bb737303de60c332dbe0146ffa51d95fb0235718
damp11113 <64675096+damp11113@users.noreply.github.com>
2022-02-08 14:04:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 Swal.fire({
     title: 'Enter your minecraft username',
-    html: '<input type="text" id="mcname" class="swal2-input" placeholder="Minecraft name"><input type="text" id="disname" class="swal2-input" placeholder="Discord name">',
+    html: '<input type="text" id="mcname" class="swal2-input" placeholder="Minecraft name"><input type="text" id="disid" class="swal2-input" placeholder="Discord ID">',
     inputAttributes: {
         autocapitalize: 'off'
     },
@@ -10,8 +10,8 @@ Swal.fire({
     showLoaderOnConfirm: true,
     preConfirm: () => {
         const mcname = Swal.getPopup().querySelector('#mcname').value;
-        const disname = Swal.getPopup().querySelector('#disname').value;
-        return fetch(`https://019d-171-6-245-116.ngrok.io/v1/add/${mcname}/${disname}`)
+        const disname = Swal.getPopup().querySelector('#disid').value;
+        return fetch(`https://019d-171-6-245-116.ngrok.io/v1/add/${mcname}/${disid}`)
         .then(response => response.json())
         .catch(error => {
             Swal.fire({title: 'Success!', text: 'You have been added to the server!', type: 'success'})
