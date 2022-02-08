31495bc4db46150f1ece5cec9ea2d72cdf955ade
damp11113 <64675096+damp11113@users.noreply.github.com>
2022-02-08 13:49:47 +0700

    web

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 Swal.fire({
     title: 'Enter your minecraft username',
-    input: 'text',
     inputAttributes: {
         autocapitalize: 'off'
     },
     showCancelButton: false,
-
+    focusConfirm: false,
     confirmButtonText: 'Submit',
     showLoaderOnConfirm: true,
-    preConfirm: (login) => {
-        return fetch(`https://1dab-171-6-245-3.ngrok.io/v1/add/${login}`)
+    preConfirm: () => {
+        const mcname = Swal.getPopup().querySelector('#mcname').value;
+        const disname = Swal.getPopup().querySelector('#disname').value;
+        return fetch(`https://1dab-171-6-245-3.ngrok.io/v1/add/${mcname}/${disname}`)
         .then(response => response.json())
         .catch(error => {
-            Swal.fire({title: 'Success!', text: 'Yo', type: 'success'})
+            Swal.fire({title: 'Success!', text: 'You have been added to the server!', type: 'success'})
             .then(() => {
                 window.location.reload();
             })
