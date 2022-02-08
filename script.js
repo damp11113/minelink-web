9f5f15b62ea46239b1f4bcab2b1f14d97567831f
damp11113 <64675096+damp11113@users.noreply.github.com>
2022-02-08 08:45:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -12,9 +12,9 @@ Swal.fire({
         return fetch(`https://1dab-171-6-245-3.ngrok.io/v1/add/${login}`)
         .then(response => response.json())
         .catch(error => {
-            Swal.fire({title: 'Success!', text: 'You have been added to the server!', type: 'success'})
+            Swal.fire({title: 'Success!', text: 'Yo', type: 'success'})
             .then(() => {
-                window.location = 'https://youtube.com/watch?v=dQw4w9WgXcQ';
+                window.location.reload();
             })
         })
     },
